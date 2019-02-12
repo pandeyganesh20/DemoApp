9520deab6f34ea1fd0c7ba71c2f4b077d8dbc091
Ganesh Pandey <pandey.ganesh20@yahoo.com>
2019-02-12 18:13:13 -0500

    I updated the view controller.

--- Ganesh/ViewController.swift
+++ Ganesh/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:
+UIViewController {
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        print("Hi!");
+        view.endEditing(true);
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //Get bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        //calculate tip and total
+        let tipPercentage=[0.1,0.2,0.3]
+        
+        let tip =  bill * tipPercentage[TipControl.selectedSegmentIndex];
+        let total = bill+tip;
+        
+        //update tip and total
+        tipLabel.text = String(format: "$%.2f ", tip)
+        totalLabel.text = String(format: "$%.2f ", total)
+    }
 }
 
